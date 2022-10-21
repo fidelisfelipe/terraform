e9c88d11d75cc24c6dc86dd55c3e13fd5a1e23b4
fidelisfelipe <atosfiel@gmail.com>
2022-10-21 02:57:19 +0100

    add project terraform

---
+++ main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+# Configure the DigitalOcean Provider
+provider "digitalocean" {
+  token = var.do_token
+}
+
+# Create a new Web Droplet in the nyc2 region
+resource "digitalocean_droplet" "jenkins" {
+  image    = "ubuntu-22-04-x64"
+  name     = "jenkins-vm"
+  region   = var.region
+  size     = "s-1vcpu-1gb"
+  ssh_keys = [data.digitalocean_ssh_key.ssh_key.id]
+}
+
+data "digitalocean_ssh_key" "ssh_key" {
+  name = var.ssh_key_name
+}
+
+resource "digitalocean_kubernetes_cluster" "k8s" {
+  name   = "k8s"
+  region = var.region
+  # Grab the latest version slug from `doctl kubernetes options versions`
+  version = "1.24.4-do.0"
+
+  node_pool {
+    name       = "default"
+    size       = "s-2vcpu-2gb"
+    node_count = 2
+  }
+}
+
+variable "do_token" {
+  default = ""
+}
+
+variable "ssh_key_name" {
+  default = ""
+}
+
+variable "region" {
+  default = ""
+}
+
+output "jenkins_ip" {
+  value = digitalocean_droplet.jenkins.ipv4_address
+}
+
+resource "local_file" "foo" {
+  content  = digitalocean_kubernetes_cluster.k8s.kube_config.0.raw_config
+  filename = "kube_config.yaml"
+}
---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+do_token     = "xxxxxxxxxxxxxxxx"
+ssh_key_name = "xxxxxxxxxxx"
+region       = "nyc1"
